ba1e49e7c829b3fb7f517840f3ff9cc9905be7ef
Jas Arora <81324353+arorajas99@users.noreply.github.com>
2025-09-19 20:44:07 +0530

    Update arduino-start.cpp

--- arduino-start.cpp
+++ arduino-start.cpp
@@ -2,27 +2,27 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(9, 10);  // CE, CSN
+RF24 radio(9, 10);
 const byte address[6] = "00001";
 
-const int startSensor = 2;  // IR sensor or LDR setup
+const int ldrPin = A0;
+int threshold = 500;
 bool raceStarted = false;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(startSensor, INPUT_PULLUP);
-
   radio.begin();
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_LOW);
+  radio.setPALevel(RF24_PA_HIGH);
   radio.stopListening();
 }
 
 void loop() {
-  if (!raceStarted && digitalRead(startSensor) == LOW) { // beam broken
+  int ldrValue = analogRead(ldrPin);
+  
+  if (!raceStarted && ldrValue < threshold) { 
     const char text[] = "START";
     radio.write(&text, sizeof(text));
-    Serial.println("Sent START");
     raceStarted = true;
   }
 }
+`
